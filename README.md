7af7261400c3d6c20ae4e67e5337243df62f52e7
William LA PORTE <laport_w@etna-alternance.net>
2016-10-14 15:18:33 +0200

    ptit fix vite fait

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ARBD-api
+
+### Pour installer
+`composer install`
--- app/Env/production.php
+++ app/Env/production.php
@@ -3,6 +3,6 @@
 putenv("APPLICATION_ENV=production");
 
 putenv("CINEMAHD_DATABASE_HOST=");
-putenv("CINEMAHD_DATABASE_NAME=zoneflight");
+putenv("CINEMAHD_DATABASE_NAME=cinemahd");
 putenv("CINEMAHD_DATABASE_USER=root");
 putenv("CINEMAHD_DATABASE_PWD=");
