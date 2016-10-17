4046827c3a77eff6bb729b4fcacdf41164601cc8
Stacie KITH <kith_s@etna-alternance.net>
2016-10-17 17:31:57 +0200

    Ajout des libs pour behat

---
+++ Tests/Functional/Context/MainContext.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Features context
+ */
+class MainContext implements Context
+{
+    public function __construct()
+    {
+    }
+}
