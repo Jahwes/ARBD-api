9a4c65db359e65a9721a192e9317f671e7a2c339
Teegie <aoubid_h@etna-alternance.net>
2016-11-18 17:33:31 +0100

    ok

--- app/Env/development.php
+++ app/Env/development.php
@@ -5,7 +5,7 @@ putenv("APPLICATION_ENV=development");
 putenv("CINEMAHD_ELASTICSEARCH_HOST=http://127.0.0.1:9200/cinemahd.test");
 putenv("CINEMAHD_ELASTICSEARCH_TYPES=movie,order,room,user,price,spectator,showing,ticket,type,people");
 
-putenv("CINEMAHD_DATABASE_HOST=cinemahd-db");
+putenv("CINEMAHD_DATABASE_HOST=192.168.5.10");
 putenv("CINEMAHD_DATABASE_NAME=cinemahd");
 putenv("CINEMAHD_DATABASE_USER=root");
 putenv("CINEMAHD_DATABASE_PWD=StacieJTM4ever<3");
